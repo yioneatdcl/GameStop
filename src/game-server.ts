01665a9b5a305105c11350b3921ca1d3af9bff79
yioneatdcl <yionedcl@gmail.com>
2024-07-20 15:55:43 +1200

    Update game-server.ts

--- src/game-server.ts
+++ src/game-server.ts
@@ -7,6 +7,8 @@ import { WebSocketTransport } from '@colyseus/ws-transport';
 require('dotenv').config();
 
 const app = express();
+/* original
+
 const port = 3220;
 
 if (isNaN(port) || port < 0 || port > 65535) {
@@ -18,10 +20,23 @@ const gameServer = new Server({
   transport: new WebSocketTransport({
     server,
   }),
-});
-
+}); 
 gameServer.define('haya-game-expo', GameRoom)
 
 server.listen(port, () => {
   console.log(`Listening on ws://localhost:${port}`);
+});  */
+
+const server = new Server({
+    server: https.createServer({
+        key: fs.readFileSync("~/key.pem"),
+        cert: fs.readFileSync("~/cert.pem")
+    }, app)
 });
+
+// Your Colyseus setup code
+// e.g., server.define("room_name", YourRoomClass);
+server.define('haya-game-expo', GameRoom)
+
+server.listen(443);
+
